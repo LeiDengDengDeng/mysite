035c70fd72aeb0caacb68438110980e84b9f8c87
lt14 <592737616@qq.com>
2017-12-21 23:14:17 +0800

    init microapp

---
+++ microapp/src/main/java/com/deng/site/MicroAppApplication.java
@@ -0,0 +1,14 @@
+package com.deng.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class MicroAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroAppApplication.class, args);
+	}
+}
---
+++ microapp/src/test/java/com/deng/site/MicroAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.deng.site;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MicroAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
